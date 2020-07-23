7865efb460474db96a320ee46c937a961f0ba322
priyapsakthivel <priyapsakthivel@gmail.com>
2020-07-23 22:17:09 +0530

    amazon project

---
+++ src/main/java/fundamentals/amazon/Amazonlogin.java
@@ -0,0 +1,33 @@
+package fundamentals.amazon;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Amazonlogin {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        String emailId=scanner.nextLine();
+        String password= scanner.nextLine();
+
+        File file = new File("P:/Webdrivers/edgedriver.exe");
+        System.setProperty("webdriver.edge.driver",file.getAbsolutePath());
+
+        WebDriver webDriver = new EdgeDriver();
+        webDriver.get("https://www.amazon.in/");
+        webDriver.manage().window().maximize();
+        WebElement login = webDriver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]"));
+        login.click();
+        WebElement email=webDriver.findElement(By.id("ap_email"));
+        email.sendKeys(emailId);
+        webDriver.findElement(By.id("continue")).click();
+
+        WebElement passkey=webDriver.findElement(By.id("ap_password"));
+        passkey.sendKeys(password);
+        webDriver.findElement(By.id("signInSubmit")).click();
+    }
+}
