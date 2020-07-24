77ec751f98a8183e4902ab354fbafecabf6f67f0
priyapsakthivel <priyapsakthivel@gmail.com>
2020-07-24 23:25:58 +0530

    calci

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a= 12;
+        int b= 20;
+        int c=35;
+        int add=a+b+c;
+        int multiply=a*b;
+        int divide=c/b;
+        int subtract=a-b;
+
+        System.out.println(add);
+        System.out.println(multiply);
+        System.out.println(divide);
+        System.out.println(subtract);
+    }
+
+}
