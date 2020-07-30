245ac7a9500dd711d02cc7a015824cfb0bbdfdcf
priyapsakthivel <priyapsakthivel@gmail.com>
2020-07-30 23:51:22 +0530

    AmazonAddress

--- src/main/java/fundamentals/amazon/AmazonAddress.java
+++ src/main/java/fundamentals/amazon/AmazonAddress.java
@@ -1,7 +1,35 @@
 package fundamentals.amazon;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.io.File;
+
 public class AmazonAddress {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        File file= new File("P:/Webdrivers/edgedriver.exe");
+        System.setProperty("webdriver.edge.driver",file.getAbsolutePath());
+
+        WebDriver webDriver= new EdgeDriver();
+        webDriver.get("https://www.amazon.in/");
+        WebElement signin=webDriver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]"));
+        signin.click();
+        webDriver.manage().window().maximize();
+        Thread.sleep(2000);
+        WebElement login= webDriver.findElement(By.id("ap_email"));
+        login.sendKeys("ytbobtester@gmailcom");
+        WebElement continueButton =webDriver.findElement(By.id("continue"));
+        continueButton.click();
+        Thread.sleep(2000);
+
+        WebElement passwordButton = webDriver.findElement(By.id("ap_password"));
+        passwordButton.sendKeys("Labone2six");
+        WebElement loginButton=webDriver.findElement(By.id("signInSubmit"));
+        loginButton.click();
+        Thread.sleep(2000);
+
 
     }
 }
