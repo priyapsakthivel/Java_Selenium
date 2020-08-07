55882cf869e1c328118cf6cc0e56a8b3bb311bdc
priyapsakthivel <priyapsakthivel@gmail.com>
2020-08-07 23:19:24 +0530

    AmazonChooseSecondProductpri ce

--- src/main/java/fundamentals/amazon/AmazonSecondProductPrice.java
+++ src/main/java/fundamentals/amazon/AmazonSecondProductPrice.java
@@ -1,11 +1,11 @@
 package fundamentals.amazon;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.io.File;
+import java.util.ArrayList;
+
 
 public class AmazonSecondProductPrice {
     public static void main(String[] args) {
@@ -14,9 +14,22 @@ public class AmazonSecondProductPrice {
         WebDriver driver= new EdgeDriver();
         driver.get("https://www.amazon.in/");
         driver.manage().window().maximize();
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("table");
+        WebElement searchBox=driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("table");
+        searchBox.sendKeys(Keys.ENTER);
         JavascriptExecutor jse= (JavascriptExecutor)driver;
-        jse.executeScript("scrollTo(0,500)");
+        jse.executeScript("scrollTo(0,200)");
+        WebElement secondProduct=driver.findElement(By.xpath("//*[@id=\"anonCarousel1\"]/ol/li[2]/div/div/span/a"));
+        secondProduct.click();
+        ArrayList<String> tab=new ArrayList <String> (driver.getWindowHandles());
+        driver.switchTo().window(tab.get(1));
+        WebElement price=driver.findElement(By.id("priceblock_dealprice"));
+        String netprice=price.getText();
+
+        System.out.println(netprice);
+
+
+
 
 
 
