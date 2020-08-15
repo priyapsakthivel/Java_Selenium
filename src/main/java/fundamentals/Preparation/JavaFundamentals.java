363359510f8a7b6629a58ee49754a8b225d1f35e
priyapsakthivel <priyapsakthivel@gmail.com>
2020-08-15 23:28:32 +0530

    java fundamentals

--- src/main/java/fundamentals/Preparation/JavaFundamentals.java
+++ src/main/java/fundamentals/Preparation/JavaFundamentals.java
@@ -4,12 +4,13 @@ package fundamentals.Preparation;
     public class JavaFundamentals {
         public static void main(String[]args){
             JavaFundamentals.methodTry();
+            int returnStorage=JavaFundamentals.methodTry();
+            System.out.println(returnStorage);
+
         }
         public static int methodTry() {
             int a = 10;
             return (5);
         }
-        int returnStorage=JavaFundamentals.methodTry();
-                    System.out.println(returnStorage);
 
     }
