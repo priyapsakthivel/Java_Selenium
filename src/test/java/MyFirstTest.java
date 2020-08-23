b0334b0dc3c88e0936ca529bbebdc4109542ca63
priyapsakthivel <priyapsakthivel@gmail.com>
2020-08-23 23:37:58 +0530

    amazon test cases

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -60,4 +61,25 @@ public class MyFirstTest {
        Select list = new Select(driver.findElement(By.id("s-result-sort-select")));
        list.selectByValue("price-asc-rank");
    }
+   public void secondProductPrice() throws InterruptedException {
+        WebDriver driver= new EdgeDriver();
+    WebElement searchBox=driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("table");
+        searchBox.sendKeys(Keys.ENTER);
+    JavascriptExecutor jse= (JavascriptExecutor)driver;
+        jse.executeScript("scrollTo(0,200)");
+        Thread.sleep(2000);
+    WebElement secondProduct=driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div/div[1]/div/span[3]/div[2]/div[4]/div/span/div/div/div/div/div[2]/h2/a"));
+        secondProduct.click();
+    ArrayList<String> tab=new ArrayList <String> (driver.getWindowHandles());
+        driver.switchTo().window(tab.get(1));
+        Thread.sleep(2000);
+
+    WebElement price=driver.findElement(By.id("priceblock_saleprice"));
+    String netprice=price.getText();
+        Thread.sleep(2000);
+
+        System.out.println("price of table ="+netprice);
+        Thread.sleep(3000);
+        driver.quit();}
 }
