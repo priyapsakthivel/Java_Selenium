f24d5683ef4a4db1d2aef5d2887dd1d5efd146bf
priyapsakthivel <priyapsakthivel@gmail.com>
2020-08-24 21:45:00 +0530

    amazon test cases

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -61,6 +61,7 @@ public class MyFirstTest {
        Select list = new Select(driver.findElement(By.id("s-result-sort-select")));
        list.selectByValue("price-asc-rank");
    }
+    @Test
    public void secondProductPrice() throws InterruptedException {
         WebDriver driver= new EdgeDriver();
     WebElement searchBox=driver.findElement(By.id("twotabsearchtextbox"));
@@ -81,5 +82,27 @@ public class MyFirstTest {
 
         System.out.println("price of table ="+netprice);
         Thread.sleep(3000);
-        driver.quit();}
+        driver.quit();
+    }
+    @Test
+    public void twotabsearchtextbox() throws InterruptedException {
+        WebElement webElement = webDriver.findElement(By.id("twotabsearchtextbox"));
+        webElement.sendKeys("table");
+        webElement.sendKeys(Keys.ENTER);
+
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
+        javascriptExecutor.executeScript("scrollTo(0,400)");
+
+        WebElement productChoice = webDriver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div/div[1]/div/span[3]/div[2]/div[3]/div/span/div/div/div/div/span/a"));
+        productChoice.click();
+        Thread.sleep(2000);
+
+        ArrayList<String> tab = new ArrayList<String>(webDriver.getWindowHandles());
+        webDriver.switchTo().window(tab.get(1));
+        Thread.sleep(2000);
+        javascriptExecutor.executeScript("scrollTo(0,200)");
+        WebElement save = webDriver.findElement(By.id("sfl-button"));
+        save.click();
+    }
+
 }
