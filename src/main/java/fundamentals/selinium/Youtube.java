b4551a0fa8ab6f057139658dc1d341acc586448e
priyapsakthivel <priyapsakthivel@gmail.com>
2020-09-16 23:05:48 +0530

    Youtube

--- src/main/java/fundamentals/selinium/Youtube.java
+++ src/main/java/fundamentals/selinium/Youtube.java
@@ -7,28 +7,22 @@ import org.openqa.selenium.edge.EdgeDriver;
 import java.io.File;
 
 public class Youtube {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         File file = new File("P:/Webdrivers/msedgedriver.exe");
         System.setProperty("webdriver.edge.driver",file.getAbsolutePath());
         Youtube youtube= new Youtube();
-        WebDriver launch =youtube.launch();
-        youtube.thumbnailAccess(launch);
+        WebDriver driver =youtube.launch();
+        youtube.thumbnailAccess(driver);
     }
     public WebDriver launch(){
         WebDriver driver= new EdgeDriver();
         driver.get("https://www.youtube.com/");
         driver.manage().window().maximize();
-        return launch();
+        return driver;
     }
-    public WebDriver thumbnailAccess(WebDriver launch) throws InterruptedException {
-        launch.findElement(By.id("thumbnail")).click();
-        Thread.sleep(20000);
-        launch.quit();
+    public WebDriver thumbnailAccess(WebDriver driver) throws InterruptedException {
+        driver.findElement(By.id("thumbnail")).click();
         Thread.sleep(20000);
         return launch();
-
-
-
     }
-
 }
