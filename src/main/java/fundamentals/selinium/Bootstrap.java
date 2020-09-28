dacb0eab3995c833d9c8967f6b2122cae0bf954f
priyapsakthivel <priyapsakthivel@gmail.com>
2020-09-28 21:55:48 +0530

    Bootstrap

--- src/main/java/fundamentals/selinium/Bootstrap.java
+++ src/main/java/fundamentals/selinium/Bootstrap.java
@@ -1,11 +1,15 @@
 package fundamentals.selinium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 import java.io.File;
 
 public class Bootstrap {
     File file=new File("P:/Webdrivers/msedgedriver.exe");
     System.setProperty("webdriver.edge.driver",file.getAbsolutePath());
-
+    WebDriver webDriver= new EdgeDriver();
+    webDriver.get("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html");
 
 
 }
