9963f85a0ec12e76b8118805edc779a1ffa2d6b7
priyapsakthivel <priyapsakthivel@gmail.com>
2020-09-29 22:08:23 +0530

    Bootstrap

--- src/main/java/fundamentals/selinium/Bootstrap.java
+++ src/main/java/fundamentals/selinium/Bootstrap.java
@@ -1,15 +1,32 @@
 package fundamentals.selinium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Bootstrap {
-    File file=new File("P:/Webdrivers/msedgedriver.exe");
-    System.setProperty("webdriver.edge.driver",file.getAbsolutePath());
-    WebDriver webDriver= new EdgeDriver();
-    webDriver.get("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html");
+    public static void main(String[]args) {
+        Bootstrap bootstrap= new Bootstrap();
+        WebDriver launching=bootstrap.launch();
+        bootstrap.maximize(launching);
 
-
+    }
+    public WebDriver launch(){
+        File file = new File("P:/Webdrivers/msedgedriver.exe");
+        System.setProperty("webdriver.edge.driver", file.getAbsolutePath());
+        WebDriver webDriver = new EdgeDriver();
+        webDriver.get("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html");
+        return webDriver;
+    }
+    public WebDriver maximize(WebDriver launching){
+        launching.manage().window().maximize();
+        launching.findElement(By.id("autoclosable-btn-success")).click();
+        String  text =launching.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[1]")).getText();
+        System.out.println(text);
+        return launching;
+    }
 }
