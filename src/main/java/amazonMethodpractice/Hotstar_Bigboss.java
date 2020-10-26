3f27000dc1582c6db5923ef196bc04dbb00b6e8d
priyapsakthivel <priyapsakthivel@gmail.com>
2020-10-26 22:05:32 +0530

    Hotstart_bigboss

---
+++ src/main/java/amazonMethodpractice/Hotstar_Bigboss.java
@@ -0,0 +1,31 @@
+package amazonMethodpractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.io.File;
+
+public class Hotstar_Bigboss {
+    public static void main(String[]args){
+        Hotstar_Bigboss hotstar_serial = new Hotstar_Bigboss();
+     WebDriver launch =hotstar_serial.hotstar_Launch();
+     hotstar_serial.hotstar(launch);
+    }
+    public WebDriver hotstar_Launch(){
+        File file= new File("P:\\Webdrivers\\msedgedriver.exe");
+        System.setProperty("webdriver.edge.driver",file.getAbsolutePath());
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.hotstar.com/in");
+        driver.manage().window().maximize();
+        return driver;
+    }
+    public WebDriver hotstar(WebDriver launch){
+        WebElement search =launch.findElement(By.id("searchField"));
+        search.sendKeys("Bigboss");
+        search.sendKeys(Keys.ENTER);
+        return hotstar_Launch();
+    }
+}
