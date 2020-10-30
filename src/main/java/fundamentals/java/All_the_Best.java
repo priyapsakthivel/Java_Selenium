6cdf4b346c4ffea2c7945f3c2f37c715480d4cf1
priyapsakthivel <priyapsakthivel@gmail.com>
2020-10-30 20:54:05 +0530

    all the best

---
+++ src/main/java/fundamentals/java/All_the_Best.java
@@ -0,0 +1,11 @@
+package fundamentals.java;
+
+public class All_the_Best {
+    public static void main(String[]args){
+       All_the_Best best= new All_the_Best();
+       best.exam();
+    }
+    public void exam(){
+        System.out.println("All the best for your exam");
+    }
+}
