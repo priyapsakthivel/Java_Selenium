ace9f50873eafcfd19c2e5c03f4b16537da49324
priyapsakthivel <priyapsakthivel@gmail.com>
2020-11-21 23:26:03 +0530

    warmup

---
+++ src/main/java/fundamentals/Preparation/Warmup.java
@@ -0,0 +1,21 @@
+package fundamentals.Preparation;
+
+import java.util.Scanner;
+
+public class Warmup {
+    public static void main(String[]args){
+        Warmup obj= new Warmup();
+        obj.evenodd();
+    }
+    public void evenodd(){
+        System.out.println("please enter a number");
+        Scanner scanner = new Scanner(System.in);
+        int a= scanner.nextInt();
+        if (a%2==0){
+            System.out.println("its even");
+        }
+        else{
+            System.out.println("its odd");
+        }
+    }
+}
