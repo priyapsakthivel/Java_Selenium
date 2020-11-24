e5fc260efb7383b2dcb97e7d9644265931a6a5d1
priyapsakthivel <priyapsakthivel@gmail.com>
2020-11-24 21:11:33 +0530

    Warmup2

--- src/main/java/fundamentals/Preparation/Warmup2.java
+++ src/main/java/fundamentals/Preparation/Warmup2.java
@@ -11,12 +11,13 @@ public class Warmup2 {
         Warmup2 warmup2=new Warmup2();
         warmup2.launch();
     }
-    public void launch(){
+    public void launch() throws InterruptedException {
         File file= new File("P:\\Webdrivers\\msedgedriver.exe");
         System.setProperty("webdriver.edge.driver",file.getAbsolutePath());
         WebDriver driver= new EdgeDriver();
         driver.get("https://gaana.com/playlist/tanmay5709-gannacom");
         driver.findElement(By.id("p-list-play_all")).click();
+        Thread.sleep(2000);
 
     }
 }
