ce7f1656c317738ed1db3c87d8b749eafd158cd3
priyapsakthivel <priyapsakthivel@gmail.com>
2020-12-10 22:31:44 +0530

    warmup3

---
+++ src/main/java/fundamentals/Preparation/warmup3.java
@@ -0,0 +1,13 @@
+package fundamentals.Preparation;
+
+public class warmup3 {
+    public static void main(String[] args) {
+        int sum1=20;
+        int sum2=30;
+        int sum3=25;
+        int sum4=40;
+        int summing_up=sum1+sum2+sum3+sum4;
+        System.out.println(summing_up);
+
+    }
+}
