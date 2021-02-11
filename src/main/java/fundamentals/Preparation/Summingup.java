2961c74c8178707d786f517ef55fd7d25503af7f
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-11 19:20:58 +0530

    Summingup

--- src/main/java/fundamentals/Preparation/Summingup.java
+++ src/main/java/fundamentals/Preparation/Summingup.java
@@ -1,10 +1,13 @@
 package fundamentals.Preparation;
 
 public class Summingup {
-
+    int sum1=11;
+    int sum2=12;
     public static void main(String[]args){
-        int sum1=11;
-        int sum2=12;
+       Summingup obj = new Summingup();
+       obj.adding(obj.sum1, obj.sum2);
+    }
+    public void adding(int sum1,int sum2){
         System.out.println(sum1+sum2);
     }
 }
