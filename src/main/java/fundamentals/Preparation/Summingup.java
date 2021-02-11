0721bf1a18a5ffe092e3b34b071058060e28247f
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-11 19:08:07 +0530

    summin

---
+++ src/main/java/fundamentals/Preparation/Summingup.java
@@ -0,0 +1,9 @@
+package fundamentals.Preparation;
+
+public class Summingup {
+    int sum1;
+    int sum2;
+    public static void main(String[]args){
+        System.out.println(sum1+sum2);
+    }
+}
