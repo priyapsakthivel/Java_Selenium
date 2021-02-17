755acd4cc68d35debd4df540e4f075e57dd36147
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-17 20:24:48 +0530

    Summingup

--- src/main/java/fundamentals/java/Fundamental7.java
+++ src/main/java/fundamentals/java/Fundamental7.java
@@ -3,7 +3,7 @@ package fundamentals.java;
 public class Fundamental7 {
     public static void main(String[] args) {
         for(int a=1;a<=10;a++){
-            System.out.println(a*5);
+            System.out.println(a*4);
         }
     }
     private static void helloWorld(){
