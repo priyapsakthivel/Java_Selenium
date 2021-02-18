0e60d5677821849c27553da394d0a13c4d5aacca
priyapsakthivel <priyapsakthivel@gmail.com>
2021-02-18 22:10:50 +0530

    Summingup

---
+++ src/main/java/amazonMethodpractice/java_warmup_learn/Scanner_learn.java
@@ -0,0 +1,12 @@
+package amazonMethodpractice.java_warmup_learn;
+
+import java.util.Scanner;
+
+public class Scanner_learn {
+    public static void main(String[]args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("please enter your name");
+        String name= scanner.next();
+    }
+    public void
+}
