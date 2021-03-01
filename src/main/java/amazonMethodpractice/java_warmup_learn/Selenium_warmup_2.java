53fdbf4a9cf4be61e67de044e2b5ec011bd884d3
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-01 23:17:25 +0530

    Summingup2

--- src/main/java/amazonMethodpractice/java_warmup_learn/Selenium_warmup_2.java
+++ src/main/java/amazonMethodpractice/java_warmup_learn/Selenium_warmup_2.java
@@ -39,10 +39,7 @@ public class Selenium_warmup_2 {
         driver.findElement(By.xpath("//*[@id='gettotal']/button")).click();
     }
     public void display_message(WebDriver driver){
-
+        driver.findElement(By.id("displayvalue");
+        System.out.println(message);
     }
-    message=driver.find_element_by_id("displayvalue").text
-    print(message)
-    display_message(driver)
-
 }
