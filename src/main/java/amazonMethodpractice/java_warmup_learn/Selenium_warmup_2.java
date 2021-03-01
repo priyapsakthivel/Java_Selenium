b388ec488c180d5b910f43382c03775e9858511e
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-01 22:40:20 +0530

    Summingup2

--- src/main/java/amazonMethodpractice/java_warmup_learn/Selenium_warmup_2.java
+++ src/main/java/amazonMethodpractice/java_warmup_learn/Selenium_warmup_2.java
@@ -1,5 +1,6 @@
 package amazonMethodpractice.java_warmup_learn;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -12,10 +13,13 @@ public class Selenium_warmup_2 {
         WebDriver driver= new EdgeDriver();
         Selenium_warmup_2 obj= new Selenium_warmup_2();
         obj.launching(driver);
+        obj.popup(driver);
     }
     public void launching(WebDriver driver){
         driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
         driver.manage().window().maximize();
-
+    }
+    public void popup(WebDriver driver){
+        driver.findElement(By.id("at-cv-lightbox-close")).click();
     }
 }
