e80a78d264976ed1e7aa6d7cc08d3c28e9a22d45
priyapsakthivel <priyapsakthivel@gmail.com>
2021-03-02 20:15:26 +0530

    Summingup2

--- src/main/java/amazonMethodpractice/java_warmup_learn/Selenium_warmup_2.java
+++ src/main/java/amazonMethodpractice/java_warmup_learn/Selenium_warmup_2.java
@@ -1,5 +1,7 @@
 package amazonMethodpractice.java_warmup_learn;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -12,13 +14,20 @@ public class Selenium_warmup_2 {
         WebDriver driver= new EdgeDriver();
         Selenium_warmup_2 obj= new Selenium_warmup_2();
         obj.launching(driver);
+        obj.closeepopup(driver);
     }
     public void launching(WebDriver driver) throws InterruptedException {
         driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
         driver.manage().window().maximize();
         Thread.sleep(2000);
+        JavascriptExecutor jse =(JavascriptExecutor)driver;
+        jse.executeScript("scrollTo(0,300)");
     }
-    public void closeepopup(WebDriver driver){
-
+    public void closeepopup(WebDriver driver) throws InterruptedException {
+        driver.findElement(By.id("at-cv-lightbox-close")).click();
+        Thread.sleep(2000);
+    }
+    public void twoinputfield(WebDriver driver,int a, int b){
+        driver.findElement(By.id("sum1")).sendKeys();
     }
 }
